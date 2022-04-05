dfcf603a79403851ded5155d6e0bb9c44085c3bb
Allen Sun <sunalle184@gmail.com>
2022-04-05 17:02:14 -0400

    Return values for deflection function

--- deflection_simplified.m
+++ deflection_simplified.m
@@ -25,11 +25,11 @@ Wr_54 = 885;
 % Young's modulus
 E = 30e6;    % [psi]
 
-deflection_analysis(D1,D2,D3,D4, A,B,G,J, Mxy, E)
-deflection_analysis(D1,D2,D3,D4, A,B,G,J, Mxz, E)
+[slope_xy, defl_xy] = deflection_analysis(D1,D2,D3,D4, A,B,G,J, Mxy, E);
+[slope_xz, defl_xz] = deflection_analysis(D1,D2,D3,D4, A,B,G,J, Mxz, E);
 
 %% Function
-function deflection_analysis(D1,D2,D3,D4, A,B,G,J, BM, E)
+function [slope, defl] = deflection_analysis(D1,D2,D3,D4, A,B,G,J, BM, E)
     addpath('SFBM');
     % INPUT PARAMETERS:
     % Shaft section diameters
@@ -70,51 +70,51 @@ function deflection_analysis(D1,D2,D3,D4, A,B,G,J, BM, E)
 
     % Obtain symbolic function for integrals between A and G
     assume((loc_A < x) & (x < loc_G))
-    integral_xy_AG = int(BM) / EI_D1;
+    integral_AG = int(BM) / EI_D1;
     syms C1 C2      % integration constants
-    slope_xy_AG = (simplify(integral_xy_AG)) + C1;    %   = slope
-    defl_xy_AG = (simplify(int(slope_xy_AG))) + C2;   %   = deflection
+    slope_AG = (simplify(integral_AG)) + C1;    %   = slope
+    defl_AG = (simplify(int(slope_AG))) + C2;   %   = deflection
     % BOUNDARY CONDITION: at A, bearing acts like a pin support 
     % -> zero deflection.
-    subs_defl_xy_AG = subs(defl_xy_AG, x, loc_A);                    
-    BC_AG_nodefl = subs_defl_xy_AG == 0
-    C2 = solve(subs_defl_xy_AG == 0);
+    subs_defl_AG = subs(defl_AG, x, loc_A);                    
+    BC_AG_nodefl = subs_defl_AG == 0
+    C2 = solve(subs_defl_AG == 0);
 
     % Obtain symbolic function for integrals between J and B
     assume((loc_J < x) & (x < loc_B))
-    integral_xy_JB = int(BM) / EI_D7;
+    integral_JB = int(BM) / EI_D7;
     syms C3 C4      % integration constants
-    slope_xy_JB = (vpa(simplify(integral_xy_JB))) + C3;    %   = slope
-    defl_xy_JB = (simplify(int(slope_xy_JB))) + C4;   %   = deflection
+    slope_JB = (vpa(simplify(integral_JB))) + C3;    %   = slope
+    defl_JB = (simplify(int(slope_JB))) + C4;   %   = deflection
     % BOUNDARY CONDITION: at B, bearing acts like a pin support 
     % -> zero deflection.
-    subs_defl_xy_JB = subs(defl_xy_JB, x, loc_B);                    
-    BC_JB_nodefl = subs_defl_xy_JB == 0
-    C4 = solve(subs_defl_xy_JB == 0, C4);   % Solve for C4 in terms of C3
+    subs_defl_JB = subs(defl_JB, x, loc_B);                    
+    BC_JB_nodefl = subs_defl_JB == 0
+    C4 = solve(subs_defl_JB == 0, C4);   % Solve for C4 in terms of C3
 
     % Obtain symbolic function for integrals between G and J
     assume((loc_G < x) & (x < loc_J))
-    integral_xy_GJ = int(BM) / EI_D3;
+    integral_GJ = int(BM) / EI_D3;
     syms C5 C6      % integration constants
-    slope_xy_GJ = (vpa(simplify(integral_xy_GJ))) + C5;    %   = slope
-    defl_xy_GJ = (simplify(int(slope_xy_GJ))) + C6;   %   = deflection
+    slope_GJ = (vpa(simplify(integral_GJ))) + C5;    %   = slope
+    defl_GJ = (simplify(int(slope_GJ))) + C6;   %   = deflection
     % BOUNDARY CONDITION: at G, deflection of AG = deflection of GJ
-    subs_defl_xy_AGGJ_1 = subs(defl_xy_AG, x, loc_G);
-    subs_defl_xy_AGGJ_2 = subs(defl_xy_GJ, x, loc_G);
-    BC_AGGJ_eqdefl = subs_defl_xy_AGGJ_1 == subs_defl_xy_AGGJ_2
+    subs_defl_AGGJ_1 = subs(defl_AG, x, loc_G);
+    subs_defl_AGGJ_2 = subs(defl_GJ, x, loc_G);
+    BC_AGGJ_eqdefl = subs_defl_AGGJ_1 == subs_defl_AGGJ_2
     % BOUNDARY CONDITION: at J, deflection of GJ = deflection of JA
-    subs_defl_xy_GJJB_1 = subs(defl_xy_GJ, x, loc_J);
-    subs_defl_xy_GJJB_2 = subs(defl_xy_JB, x, loc_J);
-    BC_GJJB_eqdefl = subs_defl_xy_GJJB_1 == subs_defl_xy_GJJB_2
+    subs_defl_GJJB_1 = subs(defl_GJ, x, loc_J);
+    subs_defl_GJJB_2 = subs(defl_JB, x, loc_J);
+    BC_GJJB_eqdefl = subs_defl_GJJB_1 == subs_defl_GJJB_2
 
     % REMAINING BOUNDARY CONDITIONS
-    subs_slope_xy_AGGJ_1 = subs(slope_xy_AG, x, loc_G);
-    subs_slope_xy_AGGJ_2 = subs(slope_xy_GJ, x, loc_G);
-    BC_AGGJ_eqslope = subs_slope_xy_AGGJ_1 == subs_slope_xy_AGGJ_2
+    subs_slope_AGGJ_1 = subs(slope_AG, x, loc_G);
+    subs_slope_AGGJ_2 = subs(slope_GJ, x, loc_G);
+    BC_AGGJ_eqslope = subs_slope_AGGJ_1 == subs_slope_AGGJ_2
 
-    subs_slope_xy_GJJB_1 = subs(slope_xy_GJ, x, loc_J);
-    subs_slope_xy_GJJB_2 = subs(slope_xy_JB, x, loc_J);
-    BC_GJJB_eqslope = subs_slope_xy_GJJB_1 == subs_slope_xy_GJJB_2
+    subs_slope_GJJB_1 = subs(slope_GJ, x, loc_J);
+    subs_slope_GJJB_2 = subs(slope_JB, x, loc_J);
+    BC_GJJB_eqslope = subs_slope_GJJB_1 == subs_slope_GJJB_2
 
     coefficient_solutions = solve(BC_AG_nodefl, ...
                                   BC_JB_nodefl, ...
@@ -131,18 +131,18 @@ function deflection_analysis(D1,D2,D3,D4, A,B,G,J, BM, E)
     C6 = coefficient_solutions.C6;
 
     % Substitute the now known coefficients into slope and deflection eqns
-    defl_xy_AG = eval(defl_xy_AG);
-    defl_xy_GJ = eval(defl_xy_GJ);
-    defl_xy_JB = eval(defl_xy_JB);
-    slope_xy_AG = eval(slope_xy_AG);
-    slope_xy_GJ = eval(slope_xy_GJ);
-    slope_xy_JB = eval(slope_xy_JB);
+    defl_AG = eval(defl_AG);
+    defl_GJ = eval(defl_GJ);
+    defl_JB = eval(defl_JB);
+    slope_AG = eval(slope_AG);
+    slope_GJ = eval(slope_GJ);
+    slope_JB = eval(slope_JB);
     
     figure()
-    fplot(slope_xy_AG, [loc_A loc_G])
+    fplot(slope_AG, [loc_A loc_G])
     hold on;
-    fplot(slope_xy_GJ, [loc_G loc_J])
-    fplot(slope_xy_JB, [loc_J loc_B])
+    fplot(slope_GJ, [loc_G loc_J])
+    fplot(slope_JB, [loc_J loc_B])
     title('Slope')
     xlabel('Axial position from bearing A (in)')
     ylabel('Slope \theta (rad)')
@@ -150,13 +150,22 @@ function deflection_analysis(D1,D2,D3,D4, A,B,G,J, BM, E)
     hold off;
 
     figure()
-    fplot(defl_xy_AG, [loc_A loc_G])
+    fplot(defl_AG, [loc_A loc_G])
     hold on;
-    fplot(defl_xy_GJ, [loc_G loc_J])
-    fplot(defl_xy_JB, [loc_J loc_B])
+    fplot(defl_GJ, [loc_G loc_J])
+    fplot(defl_JB, [loc_J loc_B])
     title('Deflection')
     xlabel('Axial position from bearing A (in)')
     ylabel('Deflection y (in)')
     yline(0)
     hold off;
+
+    % Return expressions
+    syms x
+    slope = piecewise(loc_A <= x & x <= loc_G, slope_AG, ...
+                      loc_G < x & x <= loc_J, slope_GJ, ...
+                      loc_J < x & x <= loc_B, slope_JB);
+    defl = piecewise(loc_A <= x & x <= loc_G, defl_AG, ...
+                     loc_G < x & x <= loc_J, defl_GJ, ...
+                     loc_J < x & x <= loc_B, defl_JB);
 end
