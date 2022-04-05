b96b57a6ef3fe720ffedb12a7be9302cd129d026
dkim534 <56096593+dkim534@users.noreply.github.com>
2022-04-05 18:34:56 -0400

    Inch table added

--- MSE3380_Project_Key_Design.m
+++ MSE3380_Project_Key_Design.m
@@ -13,20 +13,33 @@ t_key_m = 0.375 * 0.0254;
 %Yield Strength in Pa
 S_y_key_Pa = 460000000;
 
+%Yield Strength in Psi
+S_y_key_Psi = 67000;
+
 %Shaft diameter d in inch
 d_inch = 1.625;
 d_m = d_inch * 0.0254;
 
-T = T_3_Nm;
-r = d_m/2;
+r_m = d_m/2;
+r_inch = d_inch/2;
 
-F = T/r;
+
+
+F_N = T_3_Nm/r_m;
+F_lbf = T_3_lbfin/r_inch;
 
 %Factor of safety for key
 n_key = 2;
 
-l_key = (2*F*n_key)/(t_key_m*S_y_key_Pa);
+l_key_inch = (2*F_lbf*n_key)/(t_key_inch*S_y_key_Psi);
+l_key_m = (2*F_N*n_key)/(t_key_m*S_y_key_Pa);
 
-table_variables = {'Square key side [m]'; 'Square key length [m]'};
-table_values = [t_key_m; l_key];
-key_design_table = table(table_variables, table_values)
+
+
+table_variables_SI = {'Square key side [m]'; 'Square key length [m]'};
+table_values_SI = [t_key_m; l_key_m];
+key_design_table_SI = table(table_variables_SI, table_values_SI)
+
+table_variables_inch = {'Square key side [inch]'; 'Square key length [inch]'};
+table_values_inch = [t_key_inch; l_key_inch];
+key_design_table_inch = table(table_variables_inch, table_values_inch)
